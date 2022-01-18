0a009c563ce8c9d2e6a1d6211cae39263bf440b6
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-01-18 21:39:28 +0300

    Initial Commit

---
+++ The Light (SwiftUI)/The_Light__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  The_Light__SwiftUI_App.swift
+//  The Light (SwiftUI)
+//
+//  Created by Fedor Boretskiy on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct The_Light__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
